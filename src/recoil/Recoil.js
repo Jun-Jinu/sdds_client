90a8bfda7027c4f7bf1ef8d866fa95c9dd16f904
전진우 <jinu@M2A1.local>
2022-12-29 16:07:56 +0900

    fix css

--- src/recoil/Recoil.js
+++ src/recoil/Recoil.js
@@ -1,4 +1,5 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export const visitState = atom({
     key: "visitState",
@@ -10,10 +11,15 @@ export const websiteState = atom({
     default: "test1",
 });
 
+const { persistAtom } = recoilPersist({
+    storage: sessionStorage,
+});
+
 export const tokenState = atom({
     key: "tokenState",
     default:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2LCJ1c2VyX2xvZ2luX2lkIjoic2RkcyIsImlhdCI6MTY3MDk0NTgyNSwiZXhwIjoxNjczNTM3ODI1fQ.qXb6dRiKFnotbBkamaa3D9hpM0Q21vsZ10KTz3Lae3A",
+    effects_UNSTABLE: [persistAtom],
 });
 
 export const getToken = selector({
